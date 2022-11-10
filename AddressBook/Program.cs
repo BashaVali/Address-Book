11f9dca8064ecc896820b9f179db780239295a6a
BashaVali <bashavali222@gmail.com>
2022-11-10 07:27:03 +0530

    Address Book

---
+++ AddressBook/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AddressBook
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook");
+
+            ContactDetails contactDetails = new ContactDetails();
+        }
+    }
+}
