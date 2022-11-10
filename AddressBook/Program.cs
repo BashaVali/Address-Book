541367d6d563d778fe9a0fcfd5a494625ee5a11d
BashaVali <bashavali222@gmail.com>
2022-11-10 07:31:25 +0530

    Address Book

--- AddressBook/Program.cs
+++ AddressBook/Program.cs
@@ -9,6 +9,10 @@ namespace AddressBook
             Console.WriteLine("Welcome to AddressBook");
 
             ContactDetails contactDetails = new ContactDetails();
+
+            AddContact addContact = new AddContact();
+            addContact.EnterInput();
+            addContact.ViewTheDetails();
         }
     }
 }
